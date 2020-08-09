c4b24c21f70f950522c273146dd8088239eef178
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2020-08-09 14:43:51 +0300

    main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 locals {
-	aws_key = "aws_key"
+	aws_key = "aws_key123"
 }
 
 resource "tls_private_key" "ubuntu_private_key" {
@@ -13,31 +13,130 @@ resource "tls_private_key" "ubuntu_private_key" {
 
 resource "aws_key_pair" "generated_key" {
 	key_name   = local.aws_key
-	public_key = "${tls_private_key.ubuntu_private_key.public_key_openssh}"
+	public_key = tls_private_key.ubuntu_private_key.public_key_openssh
 }
 
-resource "aws_vpc" "first-vps" {
+resource "aws_vpc" "prod-vpc" {
 	cidr_block = "10.0.0.0/16"
 	tags = {
 		Name = "production"
 	}
 }
 
-resource "aws_subnet" "subnet1" {
-  vpc_id     = aws_vpc.first-vps.id
+resource "aws_internet_gateway" "gw" {
+	vpc_id = "${aws_vpc.prod-vpc.id}"
+}
+
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = "${aws_vpc.prod-vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block 		= "::/0"
+    gateway_id 				= aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.prod-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 
   tags = {
     Name = "prod-subnet"
   }
 }
 
-# resource "aws_instance" "web" {
-# 	ami 		  = "ami-0ac80df6eff0e70b5"
-#	instance_type = "t2.micro"
-#	key_name      = "${aws_key_pair.generated_key.key_name}"
-#
-#	tags = {
-#		Name = "Ubuntu"
-#	}
-# }
+resource "aws_route_table_association" "a" {
+  subnet_id 	= aws_subnet.subnet-1.id
+  route_table_id = aws_route_table.prod-route-table.id
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
+
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = "${aws_subnet.subnet-1.id}"
+  private_ips     = ["10.0.1.50"]
+  security_groups = ["${aws_security_group.allow_web.id}"]
+
+}
+
+resource "aws_eip" "one" {
+  vpc                       = true
+  network_interface         = aws_network_interface.web-server-nic.id
+  associate_with_private_ip = "10.0.1.50"
+
+  depends_on				= [aws_internet_gateway.gw]
+}
+
+resource "aws_instance" "web-server-instance" {
+ 	ami 		  		= "ami-0ac80df6eff0e70b5"
+	instance_type 		= "t2.micro"
+	availability_zone 	= "us-east-1a"
+	key_name      		= local.aws_key
+
+	network_interface {
+		device_index = 0
+		network_interface_id = aws_network_interface.web-server-nic.id
+	}
+
+	user_data = <<-EOF
+				#!/bin/bash
+				sudo apt update -y
+				sudo apt install apache2 -y
+				sudo systemctl start apache2
+				sudo bash -c 'echo your very first web server > /var/www/html/index.html'
+				EOF
+
+	tags = {
+		Name = "Web Server"
+	}
+}
